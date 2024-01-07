91b9298a347c9debe2d237fc2df365164b6b5c89
tr-vr <140265392+tr-vr@users.noreply.github.com>
2024-01-07 13:03:20 +1100

    generalization file to test out using interactive debugge in python

---
+++ test/generalization.py
@@ -0,0 +1,12 @@
+""" Generalization of patterns """
+from math import pi, sqrt
+
+def area_square(r):
+    return r * r
+
+def area_circle(r):
+    return r * r * pi
+
+def area_hexagon(r):
+    return r * r * 3 * sqrt(3) / 2
+
