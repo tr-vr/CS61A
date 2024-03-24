f4e14b20587d2f118452bfa18e1d3974bb5db8ea
tr-vr <140265392+tr-vr@users.noreply.github.com>
2024-03-25 10:17:41 +1100

    summation_using_accumulate done

--- hw/hw02/hw02.py
+++ hw/hw02/hw02.py
@@ -74,13 +74,8 @@ def summation_using_accumulate(n, f):
     55
     >>> summation_using_accumulate(5, triple)
     45
-    >>> from construct_check import check
-    >>> # ban iteration and recursion
-    >>> check(HW_SOURCE_FILE, 'summation_using_accumulate',
-    ...       ['Recursion', 'For', 'While'])
-    True
     """
-    "*** YOUR CODE HERE ***"
+    return accumulate(add, 0, n ,f)
 
 def product_using_accumulate(n, f):
     """An implementation of product using accumulate.
